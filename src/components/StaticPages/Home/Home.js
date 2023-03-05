16578b851c4170e19997b89eecc71d328d108c43
Christopher L Ruckman <cruckman900@cardinal.wju.edu>
2023-03-05 03:00:03 -0500

    Adding static content to Home page

--- src/components/StaticPages/Home/Home.js
+++ src/components/StaticPages/Home/Home.js
@@ -1,10 +1,33 @@
 import React, { Component } from "react";
+import styles from './Home.module.css';
 import WaitPart from "../../WaitPart/WaitPart";
 
 export default class Home extends Component {
     render() {
+        setTimeout(() => {
+            document.getElementById("body").style.display = "inline-block";
+          }, 5100);
+        
         return (
-            <WaitPart />
+            <>
+                <WaitPart />
+                <div id="body" className={styles.body}>
+                    <h1>Welcome to my website!</h1>
+                    <div>I hope to populate this site with a lot of interesting things.</div>
+                    <div>
+                        One thing that you should be aware of is that I have a Login/Register link.
+                        Why would a personal website need a login?  I plan on placing things here
+                        that not all eyes coming upon my page need to see.  I'll have graphics, writings,
+                        code samples, etc that should only be seen if you are wanting that material.  I will have
+                        items that can be purchased, shared, and/or used in whole, or in part by anyone
+                        wishing to use them.
+                    </div>
+                    <div>
+                        This will also be a place where you can share materials in order to participate
+                        in things that are far larger than 1 person can partake... like building a game!
+                    </div>
+                </div>
+            </>
         )
     }
 }
--- src/components/WaitPart/WaitPart.js
+++ src/components/WaitPart/WaitPart.js
@@ -3,6 +3,10 @@ import dragon1 from '../../assets/dragon1.png';
 import styles from './WaitPart.module.css';
 
 function WaitPart() {
+  setTimeout(() => {
+    document.getElementById("welcome").style.display = "none";
+  }, 5000);
+
   return (
     <div>
       <div id="welcome">
