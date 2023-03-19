5489a921def232aff009ba0364f633f05d17b031
Christopher L Ruckman <cruckman900@cardinal.wju.edu>
2023-03-19 03:31:02 -0400

    added loading spinner

--- src/App.js
+++ src/App.js
@@ -12,8 +12,18 @@ import Weather from './Sidebar/Weather/Weather';
 import { Container, Row, Col } from 'react-bootstrap';
 import styles from './App.module.css';
 import Button from './components/UI/Button/Button';
+import dragon1 from './assets/images/dragon1.png';
 
 function App() {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 4000);
+  }, []);
+
   const [show, setShow] = useState();
 
   useEffect(() => {
@@ -21,27 +31,38 @@ function App() {
   }, []);
 
   return (
-    <div className={styles.App}>
-      <Container fluid className={styles.Container}>
-        <Row className={styles.RowBody}>
-          <Col xs="2" className={show && styles.ColLeft}>
-              { show &&
-                <div className={styles.fixed}>
-                  <TextGame className={styles.padBottom} />
-                  <Weather className={styles.padBottom} />
-                </div>
-              }
-          </Col>
-          <Col xs="9" className={`${styles.ColRight} ${styles.Col}`}>
-            <Navigation />
-            <Button className={`${styles.buttonHolder}`} onClick={() => setShow((prevState) => !prevState)}>
-              <FontAwesomeIcon icon={!show ? faChevronRight : faChevronLeft} className={styles.chevron} fontSize="1.0rem" />
-            </Button>
-          </Col>
-        </Row>
-      </Container>
-      <Footer />
-    </div>
+    <>
+    {loading ? (
+      <>
+        <div className={styles.loaderContainer}>
+          <img src={dragon1} className={styles.spinner} />
+        </div>
+        <div className={styles.loaderContainer2}>Please remain calm.<br />Operators are standing by.</div>
+      </>
+    ) : (
+      <div className={styles.App}>
+        <Container fluid className={styles.Container}>
+          <Row className={styles.RowBody}>
+            <Col xs="2" className={show && styles.ColLeft}>
+                { show &&
+                  <div className={styles.fixed}>
+                    <TextGame className={styles.padBottom} />
+                    <Weather className={styles.padBottom} />
+                  </div>
+                }
+            </Col>
+            <Col xs="9" className={`${styles.ColRight} ${styles.Col}`}>
+              <Navigation />
+              <Button className={`${styles.buttonHolder}`} onClick={() => setShow((prevState) => !prevState)}>
+                <FontAwesomeIcon icon={!show ? faChevronRight : faChevronLeft} className={styles.chevron} fontSize="1.0rem" />
+              </Button>
+            </Col>
+          </Row>
+        </Container>
+        <Footer />
+      </div>
+    )}
+    </>
   );
 }
 
