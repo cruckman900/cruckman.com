463f2baed287a5cf19184098274a3d0d5d26309c
Christopher L Ruckman <cruckman900@cardinal.wju.edu>
2023-03-23 14:11:25 -0400

    more pixel pushing.  adding content

--- src/components/StaticPages/GitHubLinks/GitHubLinks.js
+++ src/components/StaticPages/GitHubLinks/GitHubLinks.js
@@ -1,6 +1,7 @@
 import React from "react";
 import DefaultPage from "../../UI/DefaultPage/DefaultPage";
 import lolly_sentada2 from "../../../assets/images/lolly_sentada2.png";
+import Card from "../../UI/Card/Card";
 import classes from './GitHubLinks.module.css';
 
 function GitHubLinks() {
@@ -14,40 +15,42 @@ function GitHubLinks() {
                 I have created.
             </p>
 
-            <h6>My Marvel Comic Collection</h6>
-            <p>
-                It's an older project.  I am not sure if it will be helpful as a teaching tool.  What you could do
-                is download it and either upgrade it, or develop in it as is.  A challenging exercise for someone, would
-                be to use 'Build Variants' and code the app in Java (did something similar to this idea at Google).
-            </p>
-            <p className={classes.linkBody}>
-                Here is the link:<br />
-                <a 
-                    className={classes.href}
-                    href="https://github.com/cruckman900/MyMarvelComicCollection"
-                    target="_blank"
-                    title="My Marvel Comic Collection app"
-                >
-                    MyMarvelComicCollection
-                </a>
-            </p>
+            <Card header="My Marvel Comic Collection">
+                <p>
+                    It's an older project.  I am not sure if it will be helpful as a teaching tool.  What you could do
+                    is download it and either upgrade it, or develop in it as is.  A challenging exercise for someone, would
+                    be to use 'Build Variants' and code the app in Java (did something similar to this idea at Google).
+                </p>
+                <p className={classes.linkBody}>
+                    Here is the link:<br />
+                    <a 
+                        className={classes.href}
+                        href="https://github.com/cruckman900/MyMarvelComicCollection"
+                        target="_blank"
+                        title="My Marvel Comic Collection app"
+                    >
+                        MyMarvelComicCollection
+                    </a>
+                </p>
+            </Card>
 
-            <h6>cruckman.com</h6>
-            <p>
-                I am currently teaching myself ReactJS.  My site consists of everything that I have learned so far, and as I learn more about React,
-                I will be adding content to my site that incorporates it!  So, if you keep checking in GitHub for changes, take a look!  
-            </p>
-            <p className={classes.linkBody}>
-                Here is the link:<br />
-                <a 
-                    className={classes.href}                
-                    href="https://github.com/cruckman900/cruckman.com" 
-                    target="_blank"
-                    title="cruckman.com"
-                >
-                    cruckman.com
-                </a>
-            </p>
+            <Card header="cruckman.com">
+                <p>
+                    I am currently teaching myself ReactJS.  My site consists of everything that I have learned so far, and as I learn more about React,
+                    I will be adding content to my site that incorporates it!  So, if you keep checking in GitHub for changes, take a look!  
+                </p>
+                <p className={classes.linkBody}>
+                    Here is the link:<br />
+                    <a 
+                        className={classes.href}                
+                        href="https://github.com/cruckman900/cruckman.com" 
+                        target="_blank"
+                        title="cruckman.com"
+                    >
+                        cruckman.com
+                    </a>
+                </p>
+            </Card>
         </DefaultPage>
     );
 };
