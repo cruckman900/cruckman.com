ac55b6d7c45c12a9a42adeb3322e7808cbde9f29
Christopher L Ruckman <cruckman900@cardinal.wju.edu>
2023-03-25 12:19:32 -0400

    added a fontawesome icon

--- src/components/StaticPages/2D3DArt/RequiresLogin.js
+++ src/components/StaticPages/2D3DArt/RequiresLogin.js
@@ -1,11 +1,14 @@
 import React from "react";
-import Card from "../../UI/Card/Card";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { 
+  faHand
+} from '@fortawesome/free-solid-svg-icons';
 import classes from "./RequiresLogin.module.css";
 
 function RequiresLogin() {
     return (
         <div className={classes.requiresLogin}>
-            <div>In order to see this image, you need to be logged in.</div>
+            <FontAwesomeIcon icon={faHand} style={{color: "red"}} fontSize="1.0rem" />&nbsp;&nbsp;In order to see this image, you need to be logged in.
         </div>
     )
 }
