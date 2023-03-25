a09075e1f9bc343474868db407e19cd2e7d76df1
Christopher L Ruckman <cruckman900@cardinal.wju.edu>
2023-03-25 18:50:11 -0400

    trying to style active links in dropdown

--- src/Navigation/Navigation.js
+++ src/Navigation/Navigation.js
@@ -12,22 +12,6 @@ import styles from './Navigation.module.css';
 
 import dragon1 from '../assets/images/dragon1.png';
 
-// import Home from '../components/StaticPages/Home/Home';
-// import Resume from '../components/StaticPages/Resume/Resume';
-// import Me from '../components/StaticPages/About/Me';
-// import Friends from '../components/StaticPages/About/Friends';
-// import Portfolio from '../components/StaticPages/About/Portfolio';
-// import ThisSite from '../components/StaticPages/About/ThisSite';
-// import Art2D3D from '../components/StaticPages/2D3DArt/2D3DArt';
-// import Music from '../components/StaticPages/Music/Music';
-// import MartialArts from '../components/StaticPages/MartialArts/MartialArts';
-// import CreativeWriting from '../components/StaticPages/CreativeWriting/CreativeWriting';
-// import AudioVideo from '../components/StaticPages/AudioVideo/AudioVideo';
-// import GameDev from '../components/StaticPages/GameDev/GameDev';
-// import SoftwareDev from '../components/StaticPages/SoftwareDev/SoftwareDev';
-// import GitHubLinks from '../components/StaticPages/GitHubLinks/GitHubLinks';
-// import YouTubeLinks from '../components/StaticPages/YouTubeLinks/YouTubeLinks';
-
 const Navigation = () => {
   const expand = 'sm';
   
@@ -80,35 +64,36 @@ const Navigation = () => {
                     </Nav.Link>
                     <NavDropdown
                       menuVariant='dark'
+                      eventKey="Interests"
                       title={<span className={styles.ddlink}><FontAwesomeIcon icon={myIcons['icons']} fontSize="1.0rem" />&nbsp;&nbsp;INTERESTS</span>}
                       id={`offcanvasNavbarDropdown-expand-${expand}`}
                     >
-                      <NavDropdown.Item as={Link} to="/2D3DArt/2D3DArt" href="/2D3DArt" style={{minWidth:'250px', width: '15vw'}}>
+                      <NavDropdown.Item as={Link} to="/2D3DArt/2D3DArt" eventKey="2D/3D Art" href="/2D3DArt" style={{minWidth:'250px', width: '15vw'}}>
                         <NavItem className={styles.ddlink} icon={myIcons['image']} fontSize="1.0rem"><div className={styles.floatright}>2D/3D Art</div></NavItem>
                       </NavDropdown.Item>
-                      <NavDropdown.Item as={Link} to={"/Music/Music"} href="#Music">
+                      <NavDropdown.Item as={Link} to={"/Music/Music"} eventKey="Music" href="#Music">
                         <NavItem className={styles.ddlink} icon={myIcons['music']} fontSize="1.0rem"><div className={styles.floatright}>MUSIC (GUITAR)</div></NavItem>
                       </NavDropdown.Item>
-                      <NavDropdown.Item as={Link} to={"/MartialArts/MartialArts"} href="#MartialArts">
+                      <NavDropdown.Item as={Link} to={"/MartialArts/MartialArts"} eventKey="Martial Arts" href="#MartialArts">
                         <NavItem className={styles.ddlink} icon={myIcons['personFalling']} fontSize="1.0rem"><div className={styles.floatright}>MARTIAL ARTS</div></NavItem>
                       </NavDropdown.Item>
-                      <NavDropdown.Item as={Link} to={"/CreativeWriting/CreativeWriting"} href="#CreativeWriting">
+                      <NavDropdown.Item as={Link} to={"/CreativeWriting/CreativeWriting"} eventKey="Creative Writing" href="#CreativeWriting">
                         <NavItem className={styles.ddlink} icon={myIcons['pen']} fontSize="1.0rem"><div className={styles.floatright}>CREATIVE WRITING</div></NavItem>
                       </NavDropdown.Item>
-                      <NavDropdown.Item as={Link} to={"/AudioVideo/AudioVideo"} href="#AudioVideo">
+                      <NavDropdown.Item as={Link} to={"/AudioVideo/AudioVideo"} eventKey="Audio/Video" href="#AudioVideo">
                         <NavItem className={styles.ddlink} icon={myIcons['film']} fontSize="1.0rem"><div className={styles.floatright}>AUDIO/VIDEO</div></NavItem>
                       </NavDropdown.Item>
-                      <NavDropdown.Item as={Link} to={"/GameDev/GameDev"} href="#GameDev">
+                      <NavDropdown.Item as={Link} to={"/GameDev/GameDev"} eventkey="Game Dev" href="#GameDev">
                         <NavItem className={styles.ddlink} icon={myIcons['ghost']} fontSize="1.0rem"><div className={styles.floatright}>GAME DEV</div></NavItem>
                       </NavDropdown.Item>
-                      <NavDropdown.Item as={Link} to={"/SoftwareDev/SoftwareDev"} href="#SoftwareDev">
+                      <NavDropdown.Item as={Link} to={"/SoftwareDev/SoftwareDev"} eventKey="Software Dev" href="#SoftwareDev">
                         <NavItem className={styles.ddlink} icon={myIcons['code']} fontSize="1.0rem"><div className={styles.floatright}>SOFTWARE DEV</div></NavItem>
                       </NavDropdown.Item>
                       <NavDropdown.Divider />
-                      <NavDropdown.Item as={Link} to={"/GitHubLinks/GitHubLinks"} href="#GitHubLinks">
+                      <NavDropdown.Item as={Link} to={"/GitHubLinks/GitHubLinks"} eventKey="GitHub Links" href="#GitHubLinks">
                         <NavItem className={styles.ddlink} icon={myIcons['codeBranch']} fontSize="1.0rem">GitHub LINKS</NavItem>
                       </NavDropdown.Item>
-                      <NavDropdown.Item as={Link} to={"/YouTubeLinks/YouTubeLinks"} href="#YouTubeLinks">
+                      <NavDropdown.Item as={Link} to={"/YouTubeLinks/YouTubeLinks"} eventKey="YouTube Links" href="#YouTubeLinks">
                         <NavItem className={styles.ddlink} icon={myIcons['video']} fontSize="1.0rem">YouTube LINKS</NavItem>
                       </NavDropdown.Item>
                     </NavDropdown>
@@ -118,7 +103,7 @@ const Navigation = () => {
                       id={`offcanvasNavbarDropdown-expand-${expand}`}
                     >
                       <NavDropdown.Item as={Link} to="/About/Me" href="#Me" style={{minWidth:'250px', width: '15vw'}}>
-                        <NavItem className={styles.ddlink} icon={myIcons['image']} fontSize="1.0rem"><div className={styles.floatright}>ME</div></NavItem>
+                        <NavItem className={styles.ddlink} icon={myIcons['image']} fontSize="1.0rem"><div className={styles.floatright}>JUST ME</div></NavItem>
                       </NavDropdown.Item>
                       <NavDropdown.Item as={Link} to="/About/Friends" href="#Friends">
                         <NavItem className={styles.ddlink} icon={myIcons['image']} fontSize="1.0rem"><div className={styles.floatright}>FRIENDS and FAMILY</div></NavItem>
